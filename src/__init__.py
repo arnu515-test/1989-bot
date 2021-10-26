f438bcfe9d96c5c4b01fcc70f2ad94338d193e9f
arnu515-test <arnu515@outlook.com>
2021-10-26 17:34:03 +0000

    bruh

--- src/__init__.py
+++ src/__init__.py
@@ -15,5 +15,5 @@ async def on_message(msg):
         await msg.reply("kys")
         return
     
-    if "taiwan" in msg.content.lower or "hong kong" in msg.content.lower or "hongkong" in msg.content.lower or "tiananmen" in msg.content.lower or "1989" in msg.content.lower:
+    if "taiwan" in msg.content.lower() or "hong kong" in msg.content.lower() or "hongkong" in msg.content.lower() or "tiananmen" in msg.content.lower() or "1989" in msg.content.lower():
         await msg.reply("Dear Citizen,\nYour Internet Activity has attracted our attention. You may have posted anti-CCP thoughts, which we all know is wrong. Please refrain from doing this in the future.\n\n尊敬的市民，\n您的互联网活动引起了我们的注意。 你可能发表了反中共的想法，我们都知道这是错误的。 以后请不要这样做\n\nhttps://i.imgur.com/iIWO4hw.jpg")
