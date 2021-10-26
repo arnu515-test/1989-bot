71d47c897cf30367f1f9500e0e6074f69c642f7d
arnu515-test <arnu515@outlook.com>
2021-10-26 17:53:46 +0000

    fisherman

--- src/__init__.py
+++ src/__init__.py
@@ -36,6 +36,21 @@ async def on_message(msg):
     
     if "taiwan" in msg.content.lower() or "hong kong" in msg.content.lower() or "hongkong" in msg.content.lower() or "tiananmen" in msg.content.lower() or "1989" in msg.content.lower():
         await msg.reply("Dear Citizen,\nYour Internet Activity has attracted our attention. You may have posted anti-CCP thoughts, which we all know is wrong. Please refrain from doing this in the future.\n\n尊敬的市民，\n您的互联网活动引起了我们的注意。 你可能发表了反中共的想法，我们都知道这是错误的。 以后请不要这样做\n"+xi)
+        return
     
     if "muslim" in msg.content.lower() or "musalmaan" in msg.content.lower() or "allah" in msg.content.lower() or "bomb" in msg.content.lower():
         await msg.reply("https://i.imgur.com/ch6uLHh.png")
+        return
+    
+    if "give up" in msg.content.lower() or "fisherman" in msg.content.lower():
+        await msg.reply("https://cdn.discordapp.com/attachments/850058695467204629/902613970790281256/japanese_man_never_give_up.mp4")
+        return
+    
+    if "windows" in msg.content.lower():
+        await msg.reply("Convert to linux. My current favorite distro: https://pop.system76.com")
+        return
+    
+    if msg.content == "1984":
+        await msg.delete()
+        await msg.channel.send("get silenced lmao")
+        return
